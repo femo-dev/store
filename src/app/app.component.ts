d97cb60b36a2633a94889e7dfa0cfa2233fb4a15
Fabian <fabianmendozaospina@outlook.com>
2026-08-18 21:27:29 -0500

    Adding random images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<router-outlet />'
+  // templateUrl: './app.component.html' Otra opción si hay mas logica en el componente
 })
 export class AppComponent {
   title = 'store';
